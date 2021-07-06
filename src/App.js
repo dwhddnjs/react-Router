e9a2a0f7b4188e7dec4cf213c33773f09d5e0e74
dwhddnjs <per1215@naver.com>
2021-07-06 23:02:33 +0900

    used Link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import About from "./About";
 function App() {
   return (
     <div className="App">
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
     </div>
