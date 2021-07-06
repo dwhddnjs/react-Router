bf04b7b84b8d988be85268d4c2b18608097fef44
dwhddnjs <per1215@naver.com>
2021-07-06 23:02:00 +0900

    used Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} />
     </div>
   );
 }
