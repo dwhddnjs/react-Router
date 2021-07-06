1b058151c41e6fc195315578e2a10f21d0f50ebc
dwhddnjs <per1215@naver.com>
2021-07-06 23:31:48 +0900

    used urlparameter

---
+++ src/Profile.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const profileData = {
+  dwhddnjs: {
+    name: "이종원",
+    description: "Frontend Engineer",
+  },
+
+  homer: {
+    name: "호머 심슨",
+    description: "심슨에 나오는 아빠 역할",
+  },
+};
+
+export default function Profile({ match }) {
+  const { username } = match.params;
+  const profile = profileData[username];
+
+  if (!profile) {
+    return <div>존재하지 않는 사용자입니다</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {username}({profile.name})
+      </h3>
+      <p>{profile.description}</p>
+    </div>
+  );
+}
