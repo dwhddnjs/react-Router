e4a23be4f452eee37f0d4136846d7af705d637c6
dwhddnjs <per1215@naver.com>
2021-07-07 17:55:15 +0900

    added product.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Profile from "./Profile";
+import Product from "./Product";
 
 function App() {
   return (
@@ -14,10 +15,14 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/product">상품</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
+      <Route path="/product" component={Product} />
       <Route path="/profiles/:username" component={Profile} />
     </div>
   );
---
+++ src/Product.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Product() {
+  return (
+    <div>
+      <h1>제품</h1>
+      <p>제품을 보여주는 장소</p>
+    </div>
+  );
+}
